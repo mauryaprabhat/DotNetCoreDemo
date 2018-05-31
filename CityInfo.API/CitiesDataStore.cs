71cca4ce4554913a1bda0d490d861f33401482e7
Prabhat <prabhat.maurya@hitachiconsulting.com>
2018-05-31 12:46:42 +0530

    First Commit for .net core web api

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    ID= 1,
+                    Name ="Mumbai",
+                    Description ="FilmCity of India"
+                },
+                new CityDto
+                {
+                    ID= 2,
+                    Name="Delhi",
+                    Description="Capital of India"
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            //return new JsonResult(new List<object>()
+            //{
+            //    new { id =1, Name="New York City"},
+            //    new { id=2, Name="Mumbai"}
+            //});
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+        //[HttpGet("GetCity/{id}")]
+        //public JsonResult GetCity(int id)
+        //{
+        //     return new JsonResult(CitiesDataStore.Current.Cities.Where(x=>x.ID == id).ToList());
+        //    //return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault());
+        //}            
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var data = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.ID == id);
+            if(data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+    }
+}
